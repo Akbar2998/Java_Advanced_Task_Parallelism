a8b75e0fbc6c8bf105b7a462adb38f7a501dba4c
Akbar Maxmanazarov <60185963+Akbar2998@users.noreply.github.com>
2024-04-04 17:42:23 +0200

    Create List_filling_parallell_process.java
    Created 3 unique lists and filled them with numbers and also created threads respectively. Just a for the simulation

---
+++ List_filling_parallell_process.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+    List<Integer> list3 = new ArrayList<>();
+    Thread thread1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000000; i++) {
+                list1.add(i);
+            }
+        }
+    });
+    Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    list2.add(i);
+                }
+            }
+        });
+    Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    list3.add(i);
+                }
+            }
+        });
+    thread1.start();
+    thread2.start();
+    thread3.start();
+    //We can add methods like join to control the main flow in task process 
+    System.out.println(list1.size());
+    System.out.println(list1.size());
+    System.out.println(list1.size());
+
+
+    }
+}
